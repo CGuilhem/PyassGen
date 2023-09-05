daeffd7ac4209f87217fb34e90aefe905067c8e9
Guilhem <guilhemcalonne@yahoo.fr>
2023-09-05 22:29:34 +0200

    ✨ - Add test classes  for httpApi and generatePassword

---
+++ api/test_httpApi.py
@@ -0,0 +1,31 @@
+import unittest
+from api import app  # Replace 'your_flask_app' with the actual name of your Flask app
+from httpApi import send_http_error, send_http_response
+
+class TestFlaskFunctions(unittest.TestCase):
+
+    def test_send_http_error(self):
+        # Create a test Flask app context
+        with app.test_request_context():
+            error_message = "Test error message"
+            status_code = 404
+
+            response, code = send_http_error(error_message, status_code)
+
+            # Check if the response JSON contains the error message and status code
+            self.assertEqual(code, status_code)
+            self.assertEqual(response.json['error'], error_message)
+
+    def test_send_http_response(self):
+        # Create a test Flask app context
+        with app.test_request_context():
+            test_response = "Test response message"
+
+            response, code = send_http_response(test_response)
+
+            # Check if the response JSON contains the response message and status code
+            self.assertEqual(code, 200)
+            self.assertEqual(response.json['response'], test_response)
+
+if __name__ == '__main__':
+    unittest.main()
--- api/test_password.py
+++ api/test_password.py
@@ -1,1 +1,25 @@
-def test_generatePassword():
+import unittest
+import string
+from password import generate_password  # Make sure to adjust the import to match your file structure
+
+class TestGeneratePassword(unittest.TestCase):
+
+    def test_generate_password(self):
+        # Test with a password length of 8 characters, including numbers and symbols
+        password = generate_password(8, include_numbers=True, include_symbols=True)
+        self.assertEqual(len(password), 8)  # Check length
+        self.assertTrue(any(c.isdigit() for c in password))  # Check for the presence of digits
+        self.assertTrue(any(c in string.punctuation for c in password))  # Check for the presence of symbols
+
+        # Test with a password length of 12 characters, without numbers or symbols
+        password = generate_password(12, include_numbers=False, include_symbols=False)
+        self.assertEqual(len(password), 12)  # Check length
+        self.assertFalse(any(c.isdigit() for c in password))  # Check for the absence of digits
+        self.assertFalse(any(c in string.punctuation for c in password))  # Check for the absence of symbols
+
+        # Test with an invalid password length (0 characters)
+        with self.assertRaises(ValueError):
+            generate_password(0, include_numbers=True, include_symbols=True)
+
+if __name__ == '__main__':
+    unittest.main()
