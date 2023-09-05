f77ba9cc49e5e022eefcd9f1c1c38772be9197bd
Guilhem <guilhemcalonne@yahoo.fr>
2023-09-05 15:46:43 +0200

    ✨ - Add route sendPassword

--- api.py
+++ api.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from httpApi import send_http_response, send_http_error
+from password import generate_password
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Welcome to PYassGen!</p>"
+
+@app.route("/password/<int:length>/<include_numbers>/<include_symbols>")
+def send_password(length, include_numbers, include_symbols):
+    password = generate_password(length, include_numbers, include_symbols)
+
+    if (password == None):
+      return send_http_error("La longueur du mot de passe doit être supérieure à zéro.", 400)
+    else:
+      response = {
+          'password': password
+      }
+      return send_http_response(response)
--- password.py
+++ password.py
@@ -1,23 +1,19 @@
 import random
 import string
-from http import sendHttpError, sendHttpResponse
+from httpApi import send_http_error, send_http_response
 
-def generatePassword(length, include_numbers=True, include_symbols=True):
+def generate_password(length, include_numbers, include_symbols):
     characters = string.ascii_letters  # Lettres majuscules et minuscules par défaut
 
+    if length < 1:
+        return None
+    
     if include_numbers:
         characters += string.digits  # Ajoutez des chiffres
 
     if include_symbols:
         characters += string.punctuation  # Ajoutez des symboles
 
-    if length < 1:
-        sendHttpError("La longueur du mot de passe doit être supérieure à zéro.", 400)
-
     password = ''.join(random.choice(characters) for _ in range(length))
-    response = {
-        'password': password,
-        'status_code': 200
-    }
-    sendHttpResponse(response) 
 
+    return password
