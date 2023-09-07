dd8545c92ea87eefa4f0571e80a6cbd979b7e893
Thomas1301 <Thomas1301@users.noreply.github.com>
2023-09-07 09:43:59 +0200

    ✨ - Modification of the hash

--- api/password.py
+++ api/password.py
@@ -15,18 +15,25 @@ def generate_password(length, include_numbers, include_symbols):
 
     if (include_symbols == '1'):
         characters += string.punctuation  # Ajoutez des symboles
-
+    
     password = ''.join(random.choice(characters) for _ in range(length))
 
     return password
 
-def hash_password(password):
+def hash_password(password, isSalted):
     # Create a salt (a random value) to add to the password before hashing
-    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    salt = "CeciEstPourMontrerQueLeSaltFonctionne"
     # Add the salt to the password and hash the result
-    password_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    if(isSalted):
+        password_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
+    else:
+         password_hash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), 100000)
     # Convert the binary hash to a hexadecimal representation
     password_hash = binascii.hexlify(password_hash)
     # Combine the salt and hashed password as a single string
     hashed_password = (salt + password_hash).decode('ascii')
     return hashed_password
+
+print("que des lettres :" + generate_password(50,0,0))
+print("que des lettres+chiffres :" + generate_password(50,1,0))
+print("que des lettres+symb :" + generate_password(50,0,1))
