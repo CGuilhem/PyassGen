6badd4eeb286f30dd677730bb5e9ac4ff9bbebe4
Thomas1301 <Thomas1301@users.noreply.github.com>
2023-09-07 10:32:28 +0200

    ✨ - Add the rating test class

--- api/test_password.py
+++ api/test_password.py
@@ -1,30 +1,35 @@
 import unittest
 import string
-# from api import app  
 from password import generate_password
-from password import hash_password  # Replace 'your_password_module' with the actual module name
+from password import hash_password  
+from password import evaluate_password  
 
 class TestPasswordHashing(unittest.TestCase):
 
-    def test_hash_password(self):
-        # Test hashing a password
+    def test_hash_password_with_salt(self):
+        # Test hashing a password with salt
         password = "my_secure_password"
-        hashed_password = hash_password(password)
+        hashed_password = hash_password(password, 1)
 
         # Check if the hashed password is not equal to the plain-text password
-        print("#### Hash #### Test status : Check if the hashed password is not equal to the plain-text password.")
         self.assertNotEqual(hashed_password, password)
 
         # Test hashing the same password again (should produce different hashes)
-        rehashed_password = hash_password(password)
+        rehashed_password = hash_password(password, 1)
 
         # Check if the rehashed password is not equal to the plain-text password
-        print("#### Hash #### Test status : Check if the rehashed password is not equal to the plain-text password.")
         self.assertNotEqual(rehashed_password, password)
 
         # Check if the rehashed password is not equal to the original hashed password
-        print("#### Hash #### Test status : Check if the rehashed password is not equal to the original hashed password.")
         self.assertNotEqual(rehashed_password, hashed_password)
+
+    def test_hash_password_without_salt(self):
+        # Test hashing a password without salt
+        password = "my_secure_password"
+        hashed_password = hash_password(password, 0)
+
+        # Check if the hashed password is not equal to the plain-text password
+        self.assertNotEqual(hashed_password, password)
 
 class TestPasswordFunction(unittest.TestCase):
 
@@ -60,6 +65,23 @@ class TestPasswordFunction(unittest.TestCase):
         password_Negative = generate_password(-5, include_numbers=True, include_symbols=True)
         print("#### Generate password #### Test status : Test with an invalid password length (negative number of characters).")
         self.assertEqual(password_Negative, None)
-        
+
+class TestEvaluatePassword(unittest.TestCase):
+
+    def test_weak_password(self):
+        password = "weakpass"
+        rating = evaluate_password(password)
+        self.assertEqual(rating, "Faible")
+
+    def test_medium_password(self):
+        password = "Medium123"
+        rating = evaluate_password(password)
+        self.assertEqual(rating, "Moyen")
+
+    def test_strong_password(self):
+        password = "Very_StrongPPPPP@ss123"
+        rating = evaluate_password(password)
+        self.assertEqual(rating, "Fort")
+
 if __name__ == '__main__':
     unittest.main()
